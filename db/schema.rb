d73c79588eb5bad6dfaf23bc6d94d3acd18b5b9a
Stephanie Lauziere <salauzie@gmail.com>
2015-04-14 00:31:18 -0400

    made account info updatable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414032052) do
+ActiveRecord::Schema.define(version: 20150414041343) do
 
   create_table "clinic_doctors", force: :cascade do |t|
     t.integer  "clinic_id",  limit: 4
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20150414032052) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
