685eb9c5d39321568b1d7a87d66e2cccba78a412
Stephanie Lauziere <salauzie@gmail.com>
2015-04-22 11:07:44 -0400

    added to routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' } do
-    get 'users/sign_out' => 'sessions#destroy', :as => :destroy_user_session
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
